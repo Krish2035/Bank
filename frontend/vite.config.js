aeab41cab58d18393b182f3fa435d6027b71de82
Krish Padshala <krishpadshala2035@gmail.com>
2026-04-04 17:12:02 +0530

    final commit 6

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+    commonjsOptions: {
+      include: [/node_modules/],
+    },
+  },
 })
